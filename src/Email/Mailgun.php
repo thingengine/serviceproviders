246cf438999b2027f270ed614a56cff544cb0c46
Trent Ramseyer <trentramseyer@gmail.com>
2019-06-25 13:47:23 -0700

    Added Mailgun
    Mailgun sdk

---
+++ src/Email/Mailgun.php
@@ -0,0 +1,17 @@
+<?PHP
+declare(strict_types=1);
+/*
+ * (c) ThingEngine Team
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+namespace ThingEngine\ServiceProviders\Email;
+
+
+
+class Mailgun
+{
+    public function __construct(){
+    }
+}
