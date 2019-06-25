622c295e8d094273085a4f1d7a9390cdc9c23e98
Trent Ramseyer <trentramseyer@gmail.com>
2019-06-25 13:35:33 -0700

    Initial Commit
    Skeleton

---
+++ src/Video/Twilio.php
@@ -0,0 +1,18 @@
+<?PHP
+declare(strict_types=1);
+/*
+ * (c) ThingEngine Team
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+namespace ThingEngine\ServiceProviders\Video;
+
+
+
+class Twilio
+{
+    public function __construct(){
+    }
+
+}
